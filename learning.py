eec2a1c3c65ae5a808220ae1325c95e16f023144
esuo <syedmdghouse02@gmail.com>
2026-07-30 22:39:31 +0530

    quotes generator and leaning

--- learning.py
+++ learning.py
@@ -3,5 +3,5 @@ print("Hello Ghouse")
 def calc_add(a, b):
     return b + b
 
-# works perfectly fine
-print(f"Result of Addition: {calc(1, 5)}")
+# works perfectly fine without any issue
+print(f"Result of Addition: {calc(1, 5)} is the result")
